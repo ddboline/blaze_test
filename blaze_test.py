7e15eea67ff63b6fb0720021ae3145beaf8f3f53
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-10 19:40:37 -0400

    ...

--- blaze_test.py
+++ blaze_test.py
@@ -16,7 +16,7 @@ def blaze_test():
                 print d, type(getattr(db, d))
     df = db.teams
     print df.columns
-    into.into('temp.csv', 'postgresql://ddboline:BQGIvkKFZPejrKvX@192.168.0.100:5432/lahman2014::teams')
+    into.into('temp_*.csv', 'postgresql://ddboline:BQGIvkKFZPejrKvX@192.168.0.100:5432/lahman2014')
 
 if __name__ == '__main__':
     blaze_test()
