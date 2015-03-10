3c18387651430e550f71c056609b37a42c9ab873
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-10 19:43:51 -0400

    ...

--- blaze_test.py
+++ blaze_test.py
@@ -10,13 +10,15 @@ USER = os.getenv('USER')
 
 def blaze_test():
     db = bl.Data('postgresql://ddboline:BQGIvkKFZPejrKvX@192.168.0.100:5432/lahman2014')
+    tables = []
     for d in dir(db):
         if hasattr(db, d):
             if type(getattr(db, d)) == bl.expr.expressions.Field:
-                print d, type(getattr(db, d))
+                tables.append(d)
     df = db.teams
     print df.columns
-    into.into('temp_*.csv', 'postgresql://ddboline:BQGIvkKFZPejrKvX@192.168.0.100:5432/lahman2014')
+    for t in tables:
+        into.into('%s.csv' % t, 'postgresql://ddboline:BQGIvkKFZPejrKvX@192.168.0.100:5432/lahman2014::%s' % t)
 
 if __name__ == '__main__':
     blaze_test()
