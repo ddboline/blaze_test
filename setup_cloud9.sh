69330af85affe90a3fac4f633b2e6be96a25e51d
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-12 18:08:25 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -2,8 +2,10 @@
 
 sudo apt-get install -y postgresql-client
 sudo apt-get install -y python-psycopg2
-sudo apt-get install -y python-sqlalchemy python-sqlalchemy-ext
 
 sudo bash -c "echo deb http://ddbolineathome.mooo.com/deb/trusty ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-get update
-sudo apt-get install -y --force-yes ipython python-blaze python-theano python-lasagne python-nolearn python-pylearn2
+sudo apt-get install -y --force-yes ipython python-blaze 
+sudo apt-get install -y --force-yes python-theano python-lasagne 
+sudo apt-get install -y --force-yes python-nolearn python-pylearn2
+sudo apt-get install -y --force-yes python-sqlalchemy
