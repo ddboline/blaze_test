f43097a1add1d6a04b61f39aa9697c8bb1eab6fb
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-13 08:19:58 -0400

    forgot pandas

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y python-psycopg2
 
 sudo bash -c "echo deb http://ddbolineathome.mooo.com/deb/trusty ./ > /etc/apt/sources.list.d/py2deb.list"
 sudo apt-get update
-sudo apt-get install -y --force-yes ipython python-blaze 
+sudo apt-get install -y --force-yes ipython python-blaze
+sudo apt-get install -y --force-yes python-pandas
 sudo apt-get install -y --force-yes python-theano python-lasagne 
 sudo apt-get install -y --force-yes python-nolearn python-pylearn2
 sudo apt-get install -y --force-yes python-sqlalchemy
