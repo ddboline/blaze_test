d7773b97aff51032ca659e70b5c8ec5025d7becc
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-13 09:28:39 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -4,7 +4,8 @@ sudo apt-get install -y postgresql-client
 sudo apt-get install -y python-psycopg2
 
 sudo apt-get install apt-transport-https
-sudo bash -c "echo deb https://ddbolineathome.mooo.com/deb/trusty ./ > /etc/apt/sources.list.d/py2deb.list"
+sudo bash -c "echo deb http://ddbolineathome.mooo.com/deb/trusty ./ > /etc/apt/sources.list.d/py2deb.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes ipython python-blaze
 sudo apt-get install -y --force-yes python-pandas
