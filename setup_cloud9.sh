738e17e11234c3e5a013b7a864bd550771519c72
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-09 12:31:12 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y g++ python-dev
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes ipython python-blaze
-sudo apt-get install -y --force-yes python-pandas
+sudo apt-get install -y --force-yes python-pandas python-cytoolz
 sudo apt-get install -y --force-yes python-theano python-lasagne 
 sudo apt-get install -y --force-yes python-nolearn python-pylearn2
 sudo apt-get install -y --force-yes python-sqlalchemy
