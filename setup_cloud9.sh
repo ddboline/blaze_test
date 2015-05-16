b2f44bbfe5d87f1d967b090169b19c88599c52ae
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-16 10:28:20 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -6,6 +6,6 @@ sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/t
 sudo apt-get update
 sudo apt-get install -y --force-yes postgresql-client python-psycopg2 g++ \
                                     python-dev ipython python-blaze \
-                                    python-cytoolz python-pandas
+                                    python-cytoolz python-pandas python-blueprint
 
 git clone https://github.com/ContinuumIO/blaze
