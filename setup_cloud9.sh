9482f2b25c346349bd518669687776f7a5baa737
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-05 17:06:21 -0400

    blaze->devel

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -2,11 +2,9 @@
 
 sudo apt-get update
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/blaze ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes postgresql-client python-psycopg2 g++ \
-                                    python-dev ipython python-blaze \
-                                    python-cytoolz python-pandas \
-                                    python-flask
+                                    python-dev ipython python-blaze python-scikit-learn
 
 git clone https://github.com/ContinuumIO/blaze
