0f9e957ffe57651922a361deea9a251a16438659
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-16 13:59:39 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo cp -a ${HOME}/.ssh/* /root/
+sudo cp -a ${HOME}/.ssh/* /root/.ssh/
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes postgresql-client python-psycopg2 g++ \
