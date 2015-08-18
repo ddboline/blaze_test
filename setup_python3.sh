1dd18a7bc128b04e66a794750fb67bb8eb845411
Daniel Boline <ddboline@gmail.com>
2015-08-18 18:37:38 -0400

    ...

--- setup_python3.sh
+++ setup_python3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo cp -a ${HOME}/.ssh /root/
-sudo chown -R root:root /root/
+### hack...
+export LANG="C.UTF-8"
+
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes postgresql-client python3-psycopg2 g++ \
