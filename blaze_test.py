38dddfaeae2c1283be192ecbe6aa72a48d011530
Daniel Boline <ddboline@gmail.com>
2015-10-12 04:33:29 -0400

    ...

--- blaze_test.py
+++ blaze_test.py
@@ -6,19 +6,23 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
+import shlex
+import time
+from subprocess import Popen
 import pandas as pd
 import blaze as bl
 import odo
 
+
 USER = os.getenv('USER')
+HOSTNAME = os.uname()[1]
 
 
-def blaze_test():
+def blaze_test(port=5432):
     user = 'ddboline'
     pwd = 'BQGIvkKFZPejrKvX'
 #    host = 'ddbolineathome.mooo.com'
     host = 'localhost'
-    port = 5432
     dbname = 'lahman2014'
     dbstring = 'postgresql://%s:%s@%s:%s/%s' % (user, pwd, host, port, dbname)
     dbb = bl.Data(dbstring)
@@ -38,5 +42,33 @@ def blaze_test():
         print(teams_df.describe())
     return
 
+
+class OpenPostgreSQLsshTunnel(object):
+    """ Class to let us open an ssh tunnel, then close it when done """
+    def __init__(self, port=5432):
+        self.tunnel_process = 0
+        self.postgre_port = 5432
+        self.remote_port = port
+
+    def __enter__(self):
+        if HOSTNAME != 'dilepton-tower':
+            self.postgre_port = self.remote_port
+            _cmd = 'ssh -N -L localhost:%d' % self.remote_port + \
+                   ':localhost:5432 ddboline@ddbolineathome.mooo.com'
+            args = shlex.split(_cmd)
+            self.tunnel_process = Popen(args, shell=False)
+            time.sleep(5)
+        return self.postgre_port
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.tunnel_process:
+            self.tunnel_process.kill()
+        if exc_type or exc_value or traceback:
+            return False
+        else:
+            return True
+
+
 if __name__ == '__main__':
-    blaze_test()
+    with OpenPostgreSQLsshTunnel(port=5433) as pport:
+        blaze_test(port=pport)
--- test_conda.sh
+++ test_conda.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "$HOSTNAME" != "dilepton-tower" ]; then
-    ssh -N -L localhost:5432:localhost:5432 ddboline@ddbolineathome.mooo.com &
-    sleep 5
-fi
-
 python3 ./blaze_test.py > test.out
 python3 ./blaze_tutorial.py > tutorial.out
 
