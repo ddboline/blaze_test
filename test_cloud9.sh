f6007d68179bb790a876ce5f051b72d2945044e8
Daniel Boline <ddboline@gmail.com>
2016-02-19 15:00:02 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$HOSTNAME" != "dilepton-tower" ]; then
+    ssh -N -L localhost:5432:localhost:5432 ddboline@ddbolineathome.mooo.com &
+    sleep 5
+fi
+
 ./blaze_test.py > test.out
 ./blaze_tutorial.py > tutorial.out
 
