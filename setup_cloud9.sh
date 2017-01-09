a8e6b0449b473f84942557f570ad866359a24e4e
Daniel Boline <ddboline@gmail.com>
2017-01-09 06:30:35 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -4,8 +4,8 @@ sudo cp -a ${HOME}/.ssh /root/
 sudo chown -R root:root /root/
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y -o Dpkg::Options::="--force-overwrite" \
-                                    postgresql-client python-psycopg2 g++ \
+sudo apt-get install -y --force-yes postgresql-client python-psycopg2 g++ \
                                     python-sklearn python-numpy=1.\* \
                                     python-setuptools python-pandas \
                                     python-blaze
+sudo apt-get install -f -y --force-yes
