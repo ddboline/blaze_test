8359f350db3710517c02d6659acdfdb0d5e15b85
Daniel Boline <ddboline@gmail.com>
2017-01-09 06:06:38 -0500

    ...

--- setup_python3.sh
+++ setup_python3.sh
@@ -6,7 +6,7 @@ export LANG="C.UTF-8"
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes postgresql-client python3-psycopg2 g++ \
-                                    python3-dev ipython3 python3-blaze \
+                                    python3-dev python3-blaze \
                                     python3-sklearn python3-numpy=1.\* \
                                     python3-pandas
 
