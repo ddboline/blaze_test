5fc9c563b8d36c3614ad0773df43cb2001f22bca
Daniel Boline <ddboline@gmail.com>
2017-01-09 06:34:40 -0500

    ...

--- setup_python3.sh
+++ setup_python3.sh
@@ -3,7 +3,7 @@
 ### hack...
 export LANG="C.UTF-8"
 
-sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/xenial/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes postgresql-client python3-psycopg2 g++ \
                                     python3-dev python3-blaze \
